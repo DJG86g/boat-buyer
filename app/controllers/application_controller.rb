ca2f16d80ad43cedd16bed7fd3b68c77cca7219f
Daniel Gouveia <danshome@Daniels-MacBook-Pro.local>
2018-05-08 21:33:19 -0400

    fixed_indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_photo, :username, :first_name, :last_name, :date_of_birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys:
+      [:profile_photo, :username, :first_name, :last_name, :date_of_birth])
   end
 end
