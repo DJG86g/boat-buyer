9f703faf3a268f9cd45c15e6f36f7754694b94e9
Daniel Gouveia <danshome@Daniels-MacBook-Pro.local>
2018-05-09 14:22:53 -0400

    fixed_typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
   validates :username, presence: true
@@ -8,5 +6,5 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true
   validates :date_of_birth, presence: true
-  has_many :boats 
+  has_many :boats
 end
