3e829d5292cafd63b2e6ba0156c15f5aab786ea2
Daniel Gouveia <danshome@Daniels-MacBook-Pro-2.local>
2018-05-23 14:37:48 -0400

    test_update

--- spec/features/03_user_creates_new_location_spec.rb
+++ spec/features/03_user_creates_new_location_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 feature 'user can add boating location' do
   scenario 'user visits new boating location' do
     visit new_boating_location_path
-    expect(page).to have_content "BoatBuyer"
-    expect(page).to have_content "New Boating Location"
-    expect(page).to have_content "Name"
-    expect(page).to have_content "Address"
-    expect(page).to have_content "Lat long"
-    expect(page).to have_content "Gas dock"
-    expect(page).to have_content "Public launch"
-    expect(page).to have_content "Public restroom"
+    expect(page).to have_content 'BoatBuyer'
+    expect(page).to have_content 'New Boating Location'
+    expect(page).to have_content 'Name'
+    expect(page).to have_content 'Address'
+    expect(page).to have_content 'Lat long'
+    expect(page).to have_content 'Gas dock'
+    expect(page).to have_content 'Public launch'
+    expect(page).to have_content 'Public restroom'
 
-    fill_in 'Name', with: "marthas vinyard"
-    fill_in 'Address', with: "123 main st"
-    fill_in 'Lat long', with: "42.000, 80.000"
+    fill_in 'Name', with: 'marthas vinyard'
+    fill_in 'Address', with: '123 main st'
+    fill_in 'Lat long', with: '42.000, 80.000'
     check 'Gas dock'
     check 'Public launch'
     check 'Public restroom'
-    click_button "Add My Boating Location"
+    click_button 'Add My Boating Location'
   end
 
   scenario 'users misses a form field' do
